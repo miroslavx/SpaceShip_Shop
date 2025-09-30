762d02d1988c692688802381c58fbd5e6373f842
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-09-30 14:32:52 +0300

    Add files via upload

--- Shop-master/shoptar.Data/ShoptarContext.cs
+++ Shop-master/shoptar.Data/ShoptarContext.cs
@@ -1,14 +1,14 @@
-﻿
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using shoptar.Core.Domain;
 
 namespace shoptar.Data
 {
     public class ShoptarContext : DbContext
     {
-        public ShoptarContext(DbContextOptions<ShoptarContext>options)
-        :base(options) { }
+        public ShoptarContext(DbContextOptions<ShoptarContext> options)
+            : base(options) { }
 
-        public DbSet <Spaceship> Spaceships { get; set; }
+        public DbSet<Spaceship> Spaceships { get; set; }
+        public DbSet<Kindergarten> Kindergartens { get; set; }
     }
 }
