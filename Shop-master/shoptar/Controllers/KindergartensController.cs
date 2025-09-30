8ab5ce5eac873e3a30bf7978b9a3f354d9600eae
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-09-30 14:32:09 +0300

    Add files via upload

---
+++ Shop-master/shoptar/Controllers/KindergartensController.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shoptar.Core.Domain;
+using shoptar.Data;
+using shoptar.Models.Kindergartens;
+using System.Threading.Tasks;
+
+namespace shoptar.Controllers
+{
+    public class KindergartensController : Controller
+    {
+        private readonly ShoptarContext _context;
+
+        public KindergartensController(ShoptarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _context.Kindergartens
+                .Select(x => new KindergartensIndexViewModel
+                {
+                    Id = x.Id,
+                    GroupName = x.GroupName,
+                    ChildrenCount = x.ChildrenCount,
+                    KindergartenName = x.KindergartenName,
+                    TeacherName = x.TeacherName,
+                    CreatedAt = x.CreatedAt,
+                    UpdatedAt = x.UpdatedAt
+                }).ToListAsync();
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kindergarten = await _context.Kindergartens
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (kindergarten == null)
+            {
+                return NotFound();
+            }
+
+            return View(kindergarten);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Kindergarten kindergarten)
+        {
+            if (ModelState.IsValid)
+            {
+                kindergarten.Id = Guid.NewGuid();
+                kindergarten.CreatedAt = DateTime.Now;
+                kindergarten.UpdatedAt = DateTime.Now;
+
+                _context.Add(kindergarten);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kindergarten);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kindergarten = await _context.Kindergartens.FindAsync(id);
+            if (kindergarten == null)
+            {
+                return NotFound();
+            }
+            return View(kindergarten);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Kindergarten kindergarten)
+        {
+            if (id != kindergarten.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var entityToUpdate = await _context.Kindergartens.FindAsync(id);
+                    if (entityToUpdate == null)
+                    {
+                        return NotFound();
+                    }
+
+                    entityToUpdate.GroupName = kindergarten.GroupName;
+                    entityToUpdate.ChildrenCount = kindergarten.ChildrenCount;
+                    entityToUpdate.KindergartenName = kindergarten.KindergartenName;
+                    entityToUpdate.TeacherName = kindergarten.TeacherName;
+                    entityToUpdate.UpdatedAt = DateTime.Now;
+
+                    _context.Update(entityToUpdate);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Kindergartens.Any(e => e.Id == kindergarten.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kindergarten);
+        }
+
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kindergarten = await _context.Kindergartens
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (kindergarten == null)
+            {
+                return NotFound();
+            }
+
+            return View(kindergarten);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var kindergarten = await _context.Kindergartens.FindAsync(id);
+            if (kindergarten != null)
+            {
+                _context.Kindergartens.Remove(kindergarten);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Shop-master/shoptar/Controllers/SpaceshipsController.cs
+++ Shop-master/shoptar/Controllers/SpaceshipsController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using shoptar.Core.Domain; 
+using Microsoft.EntityFrameworkCore;
+using shoptar.Core.Domain;
 using shoptar.Data;
 using shoptar.Models.Spaceships;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
 
 namespace shoptar.Controllers
 {
@@ -14,9 +15,10 @@ namespace shoptar.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            var result = _context.Spaceships
+            var result = await _context.Spaceships
                 .Select(x => new SpaceshipsIndexViewModel
                 {
                     Id = x.Id,
@@ -24,9 +26,26 @@ namespace shoptar.Controllers
                     BuiltDate = x.BuiltDate,
                     TypeName = x.TypeName,
                     Crew = x.Crew
-                });
+                }).ToListAsync();
 
             return View(result);
+        }
+
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var spaceship = await _context.Spaceships
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (spaceship == null)
+            {
+                return NotFound();
+            }
+
+            return View(spaceship);
         }
 
         public IActionResult Create()
@@ -35,7 +54,7 @@ namespace shoptar.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken] 
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Spaceship spaceship)
         {
             if (ModelState.IsValid)
@@ -43,11 +62,105 @@ namespace shoptar.Controllers
                 spaceship.Id = Guid.NewGuid();
                 spaceship.CreatedAt = DateTime.Now;
                 spaceship.ModifiedAt = DateTime.Now;
+
                 _context.Add(spaceship);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(spaceship);
+        }
+
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var spaceship = await _context.Spaceships.FindAsync(id);
+            if (spaceship == null)
+            {
+                return NotFound();
+            }
+            return View(spaceship);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Spaceship spaceship)
+        {
+            if (id != spaceship.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var entityToUpdate = await _context.Spaceships.FindAsync(id);
+                    if (entityToUpdate == null)
+                    {
+                        return NotFound();
+                    }
+
+                    entityToUpdate.Name = spaceship.Name;
+                    entityToUpdate.TypeName = spaceship.TypeName;
+                    entityToUpdate.BuiltDate = spaceship.BuiltDate;
+                    entityToUpdate.Crew = spaceship.Crew;
+                    entityToUpdate.EnginePower = spaceship.EnginePower;
+                    entityToUpdate.Passengers = spaceship.Passengers;
+                    entityToUpdate.InnerVolume = spaceship.InnerVolume;
+                    entityToUpdate.ModifiedAt = DateTime.Now;
+
+                    _context.Update(entityToUpdate);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Spaceships.Any(e => e.Id == spaceship.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(spaceship);
+        }
+
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var spaceship = await _context.Spaceships
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (spaceship == null)
+            {
+                return NotFound();
+            }
+
+            return View(spaceship);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var spaceship = await _context.Spaceships.FindAsync(id);
+            if (spaceship != null)
+            {
+                _context.Spaceships.Remove(spaceship);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
